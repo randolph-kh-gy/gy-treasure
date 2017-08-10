9ed9e53b65e6a5ae631e168561174c3c7ea1dece
Randolph Huang <wolftotem4@gmail.com>
2017-08-10 14:29:11 +0800

    修正注解

--- src/Fetcher/RemoteApi/Api1680210Com/ApiPrototype/ApiGetInfo.php
+++ src/Fetcher/RemoteApi/Api1680210Com/ApiPrototype/ApiGetInfo.php
@@ -19,9 +19,9 @@ abstract class ApiGetInfo extends ApiCall
      * @param  string  $lotCode
      * @return \GyTreasure\Fetcher\RemoteApi\Api1680210Com\ApiBusiness
      *
-     * @throws \GyTreasure\Fetcher\RemoteApi\Api1680210Com\Exceptions\ApiParseException 无法分析 API 回应
-     * @throws \GyTreasure\Fetcher\RemoteApi\Api1680210Com\Exceptions\ApiErrorException API 错误
-     * @throws \GyTreasure\Fetcher\RemoteApi\Api1680210Com\Exceptions\ApiUnreachableException 無法取得 API
+     * @throws \GyTreasure\Fetcher\RemoteApi\Exceptions\ApiParseException 无法分析 API 回应
+     * @throws \GyTreasure\Fetcher\RemoteApi\Exceptions\ApiErrorException API 错误
+     * @throws \GyTreasure\Fetcher\RemoteApi\Exceptions\ApiUnreachableException 無法取得 API
      */
     public function call($issue, $lotCode)
     {
